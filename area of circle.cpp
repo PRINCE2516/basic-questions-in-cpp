5d87a942644ed038542c058cf82a75cc164ec1af
PRINCE2516 <92628404+PRINCE2516@users.noreply.github.com>
2022-11-30 17:12:19 +0530

    Create area of circle.cpp

---
+++ area of circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r,area;
+    cout<<"enter the value of r"<<endl;
+    cin>>r;
+    area=(3.14)*r*r;
+    cout<<"area of circle is :"<<area;
+    return 0;
+}
