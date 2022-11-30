56337465428a98c1cd92f06c2da9566c9102ec5b
PRINCE2516 <92628404+PRINCE2516@users.noreply.github.com>
2022-11-30 17:14:20 +0530

    Create odd even declearation.cpp

---
+++ odd even declearation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number :";
+    cin>>n;
+    if(n%2==0)
+    cout<<"this no. is even";
+    else
+    cout<<"this no. is odd";
+    return 0;
+    
+    
+    
+    
+}
