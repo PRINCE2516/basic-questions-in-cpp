ec539ed8458b58d046a086bff5020e00bf5cce96
PRINCE2516 <92628404+PRINCE2516@users.noreply.github.com>
2022-11-30 17:17:47 +0530

    Create multiply marks of 5 sub of a student with name.cpp

---
+++ multiply marks of 5 sub of a student with name.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int  s[5],m=1;char n[10];
+    for(int i=0;i<5;i++)
+    {
+      cin>>s[i];
+      m=m*s[i];
+      
+    }
+    cout<<m<<"hello world";
+        cin>>n;
+        cout<<n;
+    
+   
+
+    return 0;
+}
